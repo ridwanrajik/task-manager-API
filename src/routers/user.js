06f6fcbf4007919f3777fbe05ce40bae0dfac34c
Ridwan <ridwanraji4@gmail.com>
2023-02-16 10:14:56 +0100

    testing task endpoints

--- src/routers/user.js
+++ src/routers/user.js
@@ -112,6 +112,21 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
     res.send()
 })
 
+// router.get('/users/me/avatar', async (req, res) => {
+//     try {
+//         const user = req.user
+
+//         if (!user || !user.avatar) {
+//             throw new Error()
+//         }
+
+//         res.set('Content-Type', 'image/png')
+//         res.send(user.avatar)
+//     } catch (e) {
+//         res.status(404).send()
+//     }
+// })
+
 router.get('/users/:id/avatar', async (req, res) => {
     try {
         const user = await User.findById(req.params.id)
