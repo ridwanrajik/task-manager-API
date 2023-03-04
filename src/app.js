70d28449c471d7686c74db34fd40860e2dba073a
Ridwan <ridwanraji4@gmail.com>
2023-03-04 22:45:17 +0100

    sets up client-side for the landing page

--- src/app.js
+++ src/app.js
@@ -2,11 +2,29 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
+const path = require('path')
+const hbs = require('hbs')
 
 const app = express()
 
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+
+app.get('', (req, res) => {
+    res.render('index')
+})
 
 module.exports = app
